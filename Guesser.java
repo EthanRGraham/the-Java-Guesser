9170f164d88739fe9aed30396beb543d6dfc7047
EthanRGraham <ethan.graham@bsu.edu>
2025-10-24 18:47:02 +0000

    figured out the menu for this project. remove Menu.class and Menu.java on a later date, they are just temporary.

---
+++ Guesser.java
@@ -0,0 +1,33 @@
+import java.util.*;
+/*public class Guesser {
+
+}
+*/
+public class Guesser {
+	public static void main(String[] args) {
+		System.out.println("------------------------------------------"+ "\n");
+		System.out.println("0) Quit"+ "\n");
+		System.out.println("1) Human Guesser"+ "\n");
+		System.out.println("2) Computer Guesser" + "\n");
+		System.out.println("------------------------------------------"+ "\n");
+		System.out.println("Please Enter 0,1 Or 2."+ "\n");
+	}
+}
+
+/*public void computerGuesser {
+	int lower = 1;
+	int upper = 100;
+	int guess = getMean(lower, upper);
+	String response = "";
+	int turns = 0;
+
+	boolean keepGoing = true;
+	while (keepGoing){
+		turns++;
+		
+		System.out.println(turns + ") I guess " + guess;
+
+}
+
+*/
+
---
+++ Menu.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Menu {
+        public static void main(String[] args) {
+                System.out.println("------------------------------------------"+ "\n");
+                System.out.println("0) Quit"+ "\n");
+                System.out.println("1) Human Guesser"+ "\n");
+                System.out.println("2) Computer Guesser" + "\n");
+                System.out.println("------------------------------------------"+ "\n");
+                System.out.println("Please Enter 0,1 Or 2.");
+        }
+}
